9316cfd725f84824d3232cffc27f2cbc9ac8d166
sarusikisslaszlo <s.laci0101@gmail.com>
2021-05-27 18:10:31 +0200

    deploy

--- communication/src/environments/environment.prod.ts
+++ communication/src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
+    apiKey: "AIzaSyD8A53dcecpd6U-oyeDjAG0F09YEdNOUvE",
+      authDomain: "communication-d7f6d.firebaseapp.com",
+      projectId: "communication-d7f6d",
+      storageBucket: "communication-d7f6d.appspot.com",
+      messagingSenderId: "11672800300",
+      appId: "1:11672800300:web:460a5e04780694e1799589",
+      measurementId: "G-6869J5FLCK"
   }
 };
